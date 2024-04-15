0f14d921683b0c3c6f4a8e6ec6289da9cc041530
name <email>
2024-04-15 08:41:59 +0300

    new dots location in install script ~/gh/dots/

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-ln -sf ~/dots/.aliases         ~/.aliases
-ln -sf ~/dots/.bash_profile    ~/.bash_profile
-ln -sf ~/dots/.bashrc          ~/.bashrc
-ln -sf ~/dots/.inputrc         ~/.inputrc
-ln -sf ~/dots/.tmux.conf       ~/.tmux.conf
+ln -sf ~/gh/dots/.aliases         ~/.aliases
+ln -sf ~/gh/dots/.bash_profile    ~/.bash_profile
+ln -sf ~/gh/dots/.bashrc          ~/.bashrc
+ln -sf ~/gh/dots/.inputrc         ~/.inputrc
+ln -sf ~/gh/dots/.tmux.conf       ~/.tmux.conf
